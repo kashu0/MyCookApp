65fb3adea00064c52793696fff0ca92ee9e16921
meehy <algusdldirl@naver.com>
2021-08-17 16:01:48 +0900

    Initial commit

---
+++ app/src/main/java/com/androidstudioprojects/mycookapp/listener/OnPostListener.java
@@ -0,0 +1,8 @@
+package com.androidstudioprojects.mycookapp.listener;
+
+import com.androidstudioprojects.mycookapp.PostInfo;
+
+public interface OnPostListener {
+    void onDelete(PostInfo postInfo);
+    void onModify();
+}
